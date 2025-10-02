e54dd01079e937360e5f8e946e83cc4d5b3877e3
Facundo <f.rullobogliani@itecriocuarto.org.ar>
2025-10-01 22:38:13 -0300

    Subi los iconos, logos | listo el login con responsive solo front

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AppRoutes from "./routes/AppRoutes";
 function App() {
   const userLoggedIn = false; // ejemplo, después lo conectas con tu auth
   return (
-    <div className='flex justify-content-center align-items-center content'>
+    <div className='flex justify-content-center align-items-center content lg:h-screen '>
       <AppRoutes userLoggedIn={userLoggedIn} />
     </div>
   )
